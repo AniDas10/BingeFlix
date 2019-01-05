dfa92b0cd4039f88d7b4e106c4936497933cb0c3
AniDas10 <ani2004@gmail.com>
2019-01-06 03:28:18 +0530

    Updates

---
+++ precache-manifest.e66b46dd43271232f231690b9746123d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "30d6b1fcea77d07e966a",
+    "url": "/BingeFlix/static/css/main.d50ae377.chunk.css"
+  },
+  {
+    "revision": "30d6b1fcea77d07e966a",
+    "url": "/BingeFlix/static/js/main.30d6b1fc.chunk.js"
+  },
+  {
+    "revision": "9ea95b9816444c4d95fe",
+    "url": "/BingeFlix/static/css/1.d10874f7.chunk.css"
+  },
+  {
+    "revision": "9ea95b9816444c4d95fe",
+    "url": "/BingeFlix/static/js/1.9ea95b98.chunk.js"
+  },
+  {
+    "revision": "db8f7410997ed565342e",
+    "url": "/BingeFlix/static/js/runtime~main.db8f7410.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/BingeFlix/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/BingeFlix/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/BingeFlix/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/BingeFlix/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/BingeFlix/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "9a982d87836f1134b7d3b7408c5d079c",
+    "url": "/BingeFlix/index.html"
+  }
+];
